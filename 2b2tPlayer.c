68a89d9fe2e4a962627680d5064cb00ddd7e5673
undoaundo-ctrl <undoaundo@gmail.com>
2026-09-14 11:58:02 +0800

    Add player list and display functionality
    This file contains a list of players for a game, printed in a formatted manner.

---
+++ 2b2tPlayer.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main(void) {
+    const char *players[200] = {
+        "__Symon__", "_5_5_", "_agent_crafter_", "_cAk", "_Cej_",
+        "_hu811226_", "_Jodd_", "_mita____", "_P5", "_PearlLoader_",
+        "_Philip_J_Fry", "_PIGY_", "_T_E_X_A_S_", "_Zit", "00_raiser_",
+        "0000_TOUCH_GRASS", "00000000000000A", "0000hauseblaster",
+        "0000HauseHunter", "00BBC", "0boz", "0hStashButler", "0KAK",
+        "1_Dotshop", "100dogi", "10K_Network", "14MY", "16123", "1drw",
+        "1out", "22Whit", "28_fito", "2b2Lag", "2b2t_info",
+        "2b2tArchitect", "2b2tCrazyDave", "2b2tgirl", "2B2Tiers",
+        "2B2TiersNether", "2b2tNMC", "2b2tOperator", "2bNews",
+        "2Bot2Tea", "2fingersBTW", "2homo", "2Map", "2ZT", "3a12",
+        "3meel_saudi", "3mx", "3ngineers", "3PMC", "420autumn",
+        "420Chan", "486K", "5fy", "6666666699999999", "7_sn", "7eece",
+        "87ac", "87ac_", "92ab", "9o1n", "9Ty6OmDMn8nQ1lvy", "abc12369",
+        "AbyssV7", "Ace________", "acegraykaylin", "Admin7822",
+        "adriabama06", "AEVM", "AfricanMonk3y", "AKFireSerpent",
+        "Alcooliquette", "alexarmpitsweat", "AlphaOr", "anonymguy",
+        "Ansefoln", "ANTIAFK_0420", "Anticultural", "Anuvin",
+        "anxiousforg", "Any_Run", "anysinglemoms", "AquariusBot5",
+        "archinstaller", "Architect_88", "ArditToska", "Auriliaa",
+        "AwesomeHeroz", "axelfoli13", "Axolotl43", "B3mgi317862",
+        "BadM2ghtyPi", "BadM3ghtyPi", "BadMightyPi", "Bargoum3450",
+        "BasketOfFries", "Benjacorr", "berekaran", "bezzma", "BigDirt1995",
+        "bingobongo__", "BiomeRoamer", "Blaineee", "blarptech", "Bleekea",
+        "blinknumi", "Blizzzen", "BombMVP", "Bromazepam6mg", "BSantaa",
+        "bubba_donald", "Bug505", "BugTheBuilder", "burnyk", "C0NFESS",
+        "Cabeto5", "Cabonk", "callMeGrandaddy", "CandySlimeGirl",
+        "capbara", "CaptainDoinEr", "CardinalBot03", "CardinalBot05",
+        "CardinalBot06", "CardinalBot09", "CardinalBot10", "CardinalBot12",
+        "CardinalBot13", "CardinalBot16", "cascaseno", "CatSquadPearl",
+        "CatSquadProxy", "Chagels", "CheeseMan84", "cherryKaffn",
+        "Chewy6767", "Chewyy002", "Chewyy77", "chigumoninja",
+        "CHoMiCzeK_PL032", "ChopWheat", "CindyH", "ciroko32",
+        "clickertrained", "Cloudy2b2t", "CloudyV3", "cocy_3a_gaps",
+        "codysmile11", "CommanderBunny", "ComparatorsOnTop", "Connor16892",
+        "Copnhagen", "CraftingRhyno", "criepig007", "crystalauraplus",
+        "CuteFembot", "CuteShino", "Cxhinoox", "cylonsus", "D0BR0_X",
+        "dadou_beast", "dadou_spiralbot1", "dadou_tracker", "Daijiro47",
+        "DamagedCardinal", "Danf22", "DankCrafts", "DapperTheFox",
+        "DarkMoon2B", "DarkSwish", "Dcharlboy", "Deadpixel_Paves",
+        "Deadpixel_Smokes", "Deadreamer", "Deafbot45", "Deanfreak",
+        "DeemonWith2ez", "DeltaruneJune5th", "dhfirhqvrqiuver", "dhoe",
+        "di9l", "diddyshop", "DJjux", "DoctaTotem", "DonnerUndBlitz",
+        "doodaacaca", "doodoobob0052", "doto", "DowsonX_0", "dragonworm",
+        "Dravk", "dtrout3jr", "DuctTapeMessiah", "DupeGriefer",
+        "EconomyReset", "Edynia", "eE_e", "ElektrikAvNU"
+    };
+
+    for (int i = 0; i < 200; i++) {
+        printf("%3d. %s\n", i + 1, players[i]);
+    }
+
+    return 0;
+}
